08c9886b7e670a9e5698a4ffcb1374e48bfa15c0
whispnode <whispnode@gmail.com>
2024-11-01 20:52:22 +0300

    updated homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
+import Link from "next/link";
+
 export default function Home() {
-    return <div>hello</div>;
+    return (
+        <div className="w-full h-screen flex items-center justify-center flex-col gap-2">
+            <p className="text-center text-xl">Hello there!! 👋</p>
+            <p className="text-center">Welcome to Checkers Game</p>
+            <Link
+                href="/game"
+                type="button"
+                className="px-5 py-3 text-sm bg-white text-zinc-950 mt-5"
+            >
+                Start Game
+            </Link>
+        </div>
+    );
 }
